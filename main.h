df39816ba9f0790491fe95eac373168440d48e89
SamuelAmihere <amisam2000@gmail.com>
2023-04-25 05:53:06 -0700

    change list name

--- main.h
+++ main.h
@@ -74,7 +74,7 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
+int print_string(va_list args, char buffer[],
 	int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
